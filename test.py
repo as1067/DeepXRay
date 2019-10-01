98ab43f226da1e66d9d380131e8a600a07e93339
Cyrus Tau <cyrus.tau@gmail.com>
2019-10-01 13:35:39 -0700

    Add shuffle

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import cv2
-
+from label_reader import LabelReader
 image = cv2.imread("images/00000001_000.png")
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 image = cv2.resize(image,(448,448))
-cv2.imshow("image",image)
-cv2.waitKey(0)
+l = LabelReader()
+annotations = l.read()
